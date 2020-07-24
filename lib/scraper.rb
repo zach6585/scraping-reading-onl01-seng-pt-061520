de474315cc43c4097d778aa79bdc99b9ea248679
zachary mines <zmines@wesleyan.edu>
2020-07-24 20:59:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+puts doc.css(".headline-26OIBN").text
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+a = courses.each do |course|
+  puts course.text.strip
+end
+
+a 
